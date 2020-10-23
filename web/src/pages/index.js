e0b3adea3112cc2e22b73341e1dd6c8ccb6e513d
Juan Manuel <livonjm@gmail.com>
2020-10-23 11:05:51 -0300

    Frontend del Login

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,5 +1,7 @@
 import { HomeComprador } from './HomeComprador';
 import { Probador } from './Probador';
 import { Categoria } from './Categoria';
+import { Login } from './Login/Login';
+import { Registrar } from './Registrar/Registrar';
 
-export { HomeComprador, Probador, Categoria };
+export { HomeComprador, Probador, Categoria, Login, Registrar };
